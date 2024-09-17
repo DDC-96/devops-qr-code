7bac25c0cd1efee670dd373bc71de2924d45cfc2
Daniel Cruz <dcruzdata@gmail.com>
2024-09-17 14:55:06 -0700

    add .ignore to terraform dir

---
+++ infrastructure/main.tf
@@ -0,0 +1,84 @@
+resource "aws_vpc" "main" {
+  cidr_block           = "172.31.0.0/16"
+  enable_dns_hostnames = true
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "main" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  route {
+    cidr_block = "172.31.0.0/16"
+    gateway_id = "local"
+  }
+}
+
+# Route Table Associations
+
+resource "aws_subnet" "subnet_1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "172.31.1.0/24"
+  availability_zone       = "us-west-1a"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "subnet_2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "172.31.2.0/24"
+  availability_zone       = "us-west-1b"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "subnet_3" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "172.31.3.0/24"
+  availability_zone       = "us-west-1b"
+  map_public_ip_on_launch = true
+}
+
+module "eks" {
+  source = "terraform-aws-modules/eks/aws"
+  version = "~> 19.0"
+
+  cluster_name = "my-cluster-eks"
+  cluster_version = "1.27"
+
+  cluster_endpoint_public_access = true
+
+  vpc_id = aws_vpc.main.id
+  subnet_ids = [aws_subnet.subnet_1.id, aws_subnet.subnet_2.id, aws_subnet.subnet_3.id]
+  control_plane_subnet_ids = [aws_subnet.subnet_1.id, aws_subnet.subnet_2.id, aws_subnet.subnet_3.id]
+
+  eks_managed_node_groups = {
+    green = {
+        min_size = 1
+        max_size = 1
+        desired_size = 1
+        instance_types = ["t2.medium"]
+    }
+  }
+}
+
+resource "aws_ecr_repository" "microservice_a" {
+  name = "microservice-a"
+}
+
+resource "aws_ecr_repository" "microservice_b" {
+  name = "microservice-b"
+}
+
+output "ecr_repository_url_microservice_a" {
+  value = aws_ecr_repository.microservice_a.repository_url
+}
+
+output "ecr_repository_url_microservice_b" {
+  value = aws_ecr_repository.microservice_b.repository_url
+}
